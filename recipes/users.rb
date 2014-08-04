304b56cc91cbc50ea358d92e589d6b3c7acf6e4b
MY NAME <MY EMAIL>
2014-08-04 14:41:49 -0400

    updating recipe users

--- recipes/users.rb
+++ recipes/users.rb
@@ -1,22 +1,36 @@
 #
-# Services
+# Cookbook Name:: cobbler
+# Recipe:: users
+# Author:: Koji Tanaka (<kj.tanaka@gmail.com>)
 #
-enable_services = %w[
-              cobblerd
-              httpd
-              xinetd
-              ]
+# Copyright 2014, FutureGrid, Indiana University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-enable_services.each do |enable_service|
-  service "#{enable_service}" do
-    action :nothing
-  end
+# Make cobblerd supports restart notification
+service "cobblerd" do
+  supports :restart => true
+  action :nothing
+end
+# Make httpd supports restart notification
+service "httpd" do
+  supports :restart => true
+  action :nothing
 end
 
-#
 # Update /etc/cobbler/users.conf
-#
-cookbook_file "/etc/cobbler/users.conf" do
+cookbook_file "/etc/cobbler/users.conf.sample" do
   source "users.conf.fgusers"
   mode 0644
   owner "root"
